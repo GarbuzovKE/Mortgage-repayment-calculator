2485fcf99cf34acd3cdd0b8449a5b9088d64e3d4
Garbuzov Kirill <garbuzovke@gmail.com>
2024-09-16 14:36:38 +0300

    config fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mortgage-repayment-calculator/",
+  base: "/Mortgage-repayment-calculator/",
   plugins: [react()],
 });
